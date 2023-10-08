baf1a0158e9ea47779f75a652c27cd62bea7d3ee
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2023-10-09 00:38:02 +1100

    Add and break up navigation sidebar component

---
+++ src/components/Navigation/index.tsx
@@ -0,0 +1,18 @@
+import {
+    CalendarIcon,
+    ChartPieIcon,
+    DocumentDuplicateIcon,
+    FolderIcon,
+    HomeIcon,
+    UsersIcon,
+} from '@heroicons/react/24/outline'
+
+export * from './Navigation'
+export const navigation = [
+    { name: 'Dashboard', href: '/dashboard', icon: HomeIcon, current: true },
+    { name: 'Team', href: '#', icon: UsersIcon, current: false },
+    { name: 'Projects', href: '#', icon: FolderIcon, current: false },
+    { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
+    { name: 'Documents', href: '#', icon: DocumentDuplicateIcon, current: false },
+    { name: 'Reports', href: '#', icon: ChartPieIcon, current: false },
+]
