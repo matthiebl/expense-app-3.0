e87f42f208f3e3258bbb61d438b3d0c128692456
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2023-10-09 00:38:36 +1100

    Add navigation sidebar to app and basic example page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang='en'>
-            <body className={inter.className}>{children}</body>
+        <html lang='en' className='h-full bg-slate-900'>
+            <body className={`${inter.className} h-full`}>{children}</body>
         </html>
     )
 }
