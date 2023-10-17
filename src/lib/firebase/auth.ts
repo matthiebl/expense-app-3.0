7d7ea2f08371d91444167be1ddb6c91624a2dea0
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2023-10-17 11:27:30 +1100

    Replace supabase with firebase

---
+++ src/lib/firebase/auth.ts
@@ -0,0 +1,69 @@
+import { Auth, createUserWithEmailAndPassword, signOut, updateProfile } from 'firebase/auth'
+import { authFB } from './config'
+
+class FireAuth {
+    auth: Auth
+
+    constructor(auth: Auth) {
+        this.auth = auth
+    }
+
+    displayName() {
+        console.log(this.auth)
+        return this.auth.currentUser?.displayName || null
+    }
+
+    async signUp(email: string, password: string) {
+        try {
+            const { user } = await createUserWithEmailAndPassword(this.auth, email, password)
+            return { user }
+        } catch (error: any) {
+            return {
+                error: {
+                    code: error.code,
+                    message: error.message,
+                },
+            }
+        }
+    }
+
+    async signOut() {
+        try {
+            await signOut(this.auth)
+            return {}
+        } catch (error: any) {
+            return {
+                error: {
+                    code: error.code,
+                    message: error.message,
+                },
+            }
+        }
+    }
+
+    async updateProfile(displayName: string) {
+        if (!this.auth.currentUser) {
+            return {
+                error: {
+                    message: 'No user is signed in',
+                    code: 999,
+                },
+            }
+        }
+        try {
+            await updateProfile(this.auth.currentUser, {
+                displayName,
+            })
+            return {}
+        } catch (error: any) {
+            return {
+                error: {
+                    code: error.code,
+                    message: error.message,
+                },
+            }
+        }
+    }
+}
+
+export const auth = new FireAuth(authFB)
